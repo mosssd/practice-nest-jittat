9afaa567df2aab2e7571c9485ce87694340be9dd
saranrak <saranrak.d@ku.th>
2024-04-11 12:32:24 +0700

    show courses

--- cr-backend/src/courses.controller.ts
+++ cr-backend/src/courses.controller.ts
@@ -5,8 +5,19 @@ export class CoursesController {
 
   @Get()
   findAll(): any{
-    return {
-      massage: 'hello naja'
-    };
+    return [
+      { id: '100',
+        number:'01204111',
+        title:'computer programming'
+      },
+      { id: 'sdbb',
+        number:'01204111',
+        title:'network and data'
+      },
+      { id: 'dzfbfhb',
+        number:'01204111',
+        title:'hello ar'
+      }
+    ]
   }
 }
--- cr-frontend/src/App.tsx
+++ cr-frontend/src/App.tsx
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-
-type AppState = {
-  massage:string;
-};
-
-class App extends React.Component<{}, AppState> {
-  state: AppState = { 
-    massage: 'default massage',
-  };
-
-  componentDidMount(){
+const App = () => {
+  const [courses, setcourses] = useState<any[]>([]);
+  useEffect(() => {
     fetch('http://localhost:3000/courses')
       .then(res => res.json())
-      .then(obj => {
-        this.setState({massage: obj.massage})
+      .then(courses => {
+        console.log(courses);
+        setcourses(courses);
       })
-  };
-  
-  render() {
-    return (
-      <div>
-        {this.state.massage}
-      </div>
-    );
-  }
+  },[]);
+
+  return(
+    <div className='App'>
+      <ul>
+        {courses.map(item => (
+          <li key={item.id}>{item.number}-{item.title}</li>
+        ))}
+      </ul>
+      
+    </div>
+  );
 }
+
+// type AppState = {
+//   massage:string;
+// };
+
+// class App extends React.Component<{}, AppState> {
+//   state: AppState = { 
+//     massage: 'default massage',
+//   };
+
+//   componentDidMount(){
+//     fetch('http://localhost:3000/courses')
+//       .then(res => res.json())
+//       .then(obj => {
+//         this.setState({massage: obj.massage})
+//       })
+//   };
+  
+//   render() {
+//     return (
+//       <div>
+//         {this.state.massage}
+//       </div>
+//     );
+//   }
+// }
 
 
 // function App() {
